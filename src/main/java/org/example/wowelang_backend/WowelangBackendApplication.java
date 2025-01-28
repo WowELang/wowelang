1582427cc07fa90fb2ae98a195d9bb9a2ffefdfe
jay91537 <veuxft@gmail.com>
2025-01-29 00:45:57 +0900

    Feat : first commit

---
+++ src/main/java/org/example/wowelang_backend/WowelangBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.wowelang_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowelangBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WowelangBackendApplication.class, args);
+    }
+
+}
