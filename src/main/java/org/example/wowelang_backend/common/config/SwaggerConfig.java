f2b433280e8f5bb807975e71d62db1d00885f306
jay91537 <veuxft@gmail.com>
2025-05-22 01:55:38 +0900

    Feat : Swagger 토큰 로그인 설정

--- src/main/java/org/example/wowelang_backend/common/config/SwaggerConfig.java
+++ src/main/java/org/example/wowelang_backend/common/config/SwaggerConfig.java
@@ -1,24 +1,38 @@
 package org.example.wowelang_backend.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(title = "WOWELANG API",
-        description = "WOWELANG API SWAGGER",
-        version = "V1")
-)
+@RequiredArgsConstructor
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi api_File() {
-        return GroupedOpenApi.builder()
-                .group("와이랭 api 서버")
-                .pathsToMatch("/**")
-                .build();
+    public OpenAPI customOpenAPI() {
+        // Security Scheme 정의
+        SecurityScheme securityScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(SecurityScheme.In.HEADER)
+            .name("Authorization");
+
+        // Security Requirement 정의
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
+        return new OpenAPI()
+            .info(new Info().title("Wowelang API")
+                .description("와이랭 API 서버")
+                .version("v1.0"))
+            .addSecurityItem(securityRequirement)  // Security Requirement 추가
+            .schemaRequirement("BearerAuth", securityScheme);  // Security Scheme 추가
     }
+
 }
