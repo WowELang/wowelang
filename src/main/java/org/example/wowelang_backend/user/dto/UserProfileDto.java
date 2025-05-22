c9abc0375b71906ab70ddde8fe6868c636566aff
uoo6uoow <154813925+uoo6uoow@users.noreply.github.com>
2025-05-23 02:03:04 +0900

    Refactor : (Domain)프로필 조회 dto 데이터 추가

--- src/main/java/org/example/wowelang_backend/user/dto/UserProfileDto.java
+++ src/main/java/org/example/wowelang_backend/user/dto/UserProfileDto.java
@@ -3,6 +3,7 @@ package org.example.wowelang_backend.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.wowelang_backend.user.domain.Usertype;
 
 import java.util.List;
 
@@ -14,4 +15,8 @@ public class UserProfileDto {
     private String nickname;
     private CharacterInfoDto character;
     private List<InterestDto> interests;
+    private String name; // 이름
+    private String major; // 전공
+    private Usertype usertype; // 유저타입
+    private String country; // 국적
 }
